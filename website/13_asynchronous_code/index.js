c90d769439af0c192d00a0ecac86ec96d6aed935
theflyingraijin <pvselvam.nelson@gmail.com>
2024-07-23 22:54:47 -0400

    :sparkles: async code

---
+++ website/13_asynchronous_code/index.js
@@ -0,0 +1,10 @@
+/* ASYNCHRONOUS CODE
+
+    synchronous code - code that runs line by line sequentially. it waits on previous operation to complete if any.
+
+    asynchronous code - allows multiple operations to be performed concurrently without waiting
+                        doesn't block the execution flow & allows programs to continue
+                        (I/O operations, network requests, timers, etc.)
+                        handled with: callbacks, promises, async/await
+
+*/
