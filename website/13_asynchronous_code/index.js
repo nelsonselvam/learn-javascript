6c3680e25da0771fa8040f8201e35823ec11a714
nelsons <mypersonelson@gmail.com>
2024-07-24 00:15:07 -0400

    :sparkles: dom code

--- website/13_asynchronous_code/index.js
+++ website/13_asynchronous_code/index.js
@@ -8,3 +8,40 @@
                         handled with: callbacks, promises, async/await
 
 */
+
+/* CALLBACKS
+
+    - A callback is a function passed as an argument to another function
+
+    - This technique allows a function to call another function
+
+    - A callback function can run after another function has finished
+
+*/
+
+/* BEFORE CODE
+function func1(){
+    setTimeout(() => console.log(1),3000);
+}
+
+console.log(2);
+console.log(3);
+console.log(4);
+func1();
+
+// BEFORE CODE */
+
+function func1(callback) {
+  setTimeout(() => {
+    console.log(1);
+    callback(); // forcing the callback to have better control over the sequence
+  }, 3000);
+}
+
+function func2() {
+  console.log(2);
+  console.log(3);
+  console.log(4);
+}
+
+func1(func2);
---
+++ website/14_error_handling/index.js
@@ -0,0 +1,52 @@
+/* ERROR HANDLING
+
+    Error - An object that is created to represent a problme that occurs often with user input
+    or establishing a connection
+
+    Network Errors
+    Promise Rejection
+    Security Errors
+
+    Error Handling
+
+    try { ... }     -   Encloses the code that might potentially cause the error
+    catch { ... }   -   Catch and handle any thrown errors from try{}
+    finally { ... } -   (Optional) Always executes. Used mostly for clean-up.
+                        ex: file closure, connection closure, release resources etc.
+
+*/
+
+// example of error
+// console.lag("This will result in a type reference error");
+
+// console.log(x); // this will result in a uncaught reference error
+
+// example 1 : uncaught referene error handling
+try {
+  console.log(x);
+} catch (error) {
+  console.error(error);
+} finally {
+  console.log("clean up!! - always executes");
+}
+
+// example 2 : user input error handling
+
+try {
+  const dividend = Number(window.prompt("Enter a number to divide by"));
+  const divisor = Number(window.prompt("Enter a number to divide"));
+
+  if (divisor === 0) {
+    // manually throwing an error
+    throw new Error("You can't divide by zero!!");
+  } else if (isNaN(dividend) || isNaN(divisor)) {
+    window.alert("Please enter a valid number");
+    throw new Error("Please enter a valid number");
+  }
+  const result = dividend / divisor;
+  console.log(result);
+} catch (error) {
+  console.error(error);
+}
+
+console.log("Reached the end of the program");
